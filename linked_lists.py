16808815afacba7145b885f9a82a872ea61cfb4b
Abbi Devins-Suresh <aesther2012@gmail.com>
2021-09-05 17:28:55 -0400

    created remove method

--- linked_lists.py
+++ linked_lists.py
@@ -16,9 +16,11 @@ def create_node(val):
 node1 = create_node(2)
 node2 = create_node(3)
 node3 = create_node(4)
+node4 = create_node(4)
 
 node1.next = node2
 node2.next = node3
+node3.next = node4
 
 
 def print_linked_list(head):
@@ -37,6 +39,35 @@ def print_linked_list_in_reverse(head):
     print(currentHead)
 
 
+# remove first node with target value
+def remove_node_with_val(head, val):
+    if head is None:
+        return
+
+    node = head
+
+    if node.val == val and node.next is None:
+        return None
+
+    if node.val == val and node.next is not None:
+        head = node.next
+
+    # breakpoint()
+    while node is not None:
+        print(node.val)
+        if node.next is not None:
+            if node.next.val == val:
+                next_node = node.next.next
+                node.next = next_node
+
+        node = node.next
+
+    return head
+
+
 print_linked_list(node1)
 print("Reverse list")
 print_linked_list_in_reverse(node1)
+
+list = remove_node_with_val(node1, 4)
+print_linked_list(list)
